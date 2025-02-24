a6e97d74e8327cb2aa67794eaf3fa882f1707ef8
Felipe Víctor <felipe@oitsys.com>
2025-02-24 14:20:34 -0600

    feat(home): create landing page skeleton

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+export const HomePage = () => {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
